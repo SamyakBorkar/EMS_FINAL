269fb212fcb519a1cf1762fbba1431b4f34f964b
SamyakBorkar <2022bit037@sggs.ac.in>
2025-07-18 20:28:25 +0530

    updated render endpoint

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-    baseURL: 'https://smarthr-36g9.onrender.com',
+    baseURL: 'https://ems-final.onrender.com',
 });
 
 API.interceptors.request.use((req)=>{
